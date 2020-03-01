03121be168de49c1f2b412e0b508c16062bdf60c
cjosti <cleitonosti@gmail.com>
2020-03-01 19:22:38 -0300

    destructuring

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -9,26 +9,26 @@ import {
 } from 'react-router-dom';
 
 export default function Topics() {
-    let match = useRouteMatch();
+    let { url, path} = useRouteMatch();
   
     return (
       <div>
         <h2>Topics</h2>
         <ul>
           <li>
-            <Link to={`${match.url}/components`}>Components</Link>
+            <Link to={`${url}/components`}>Components</Link>
           </li>
           <li>
-            <Link to={`${match.url}/props-v-state`}>
+            <Link to={`${url}/props-v-state`}>
               Props v. State
             </Link>
           </li>
         </ul>
         <Switch>
-          <Route path={`${match.path}/:topicId`}>
+          <Route path={`${path}/:topicId`}>
             <Topic />
           </Route>
-          <Route path={`${match.path}`}>
+          <Route path={`${path}`}>
             <h3>Please select a topic.</h3>
           </Route>
         </Switch>

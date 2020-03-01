d7f5cc2c88088b9fbe03dce71c50cefc1fb10b26
cjosti <cleitonosti@gmail.com>
2020-03-01 19:19:21 -0300

    separate to components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  useRouteMatch,
-  useParams
+  Link
 } from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import Topics from './components/Topics';
 
 export default function App() {
   return (
@@ -47,49 +50,4 @@ export default function App() {
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
-function Topics() {
-  let match = useRouteMatch();
-
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={`${match.path}`}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-return <h3>Requested topic ID: {topicId}</h3>
 }
---
+++ src/components/Topics.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Topic from './Topic';
+
+import {
+    Route,
+    Link,
+    Switch,
+    useRouteMatch
+} from 'react-router-dom';
+
+export default function Topics() {
+    let match = useRouteMatch();
+  
+    return (
+      <div>
+        <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to={`${match.url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/props-v-state`}>
+              Props v. State
+            </Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path={`${match.path}/:topicId`}>
+            <Topic />
+          </Route>
+          <Route path={`${match.path}`}>
+            <h3>Please select a topic.</h3>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+  
